983536274e7d7411ab5016e81686abd49ccb8be1
yangjik <88581543+yangjik@users.noreply.github.com>
2023-01-09 21:07:17 +0900

    Create setting.py
    setting or tip

---
+++ setting.py
@@ -0,0 +1,19 @@
+# 아나콘다 가상환경 설정(아나콘다에서 exe파일 다운)
+
+# 가상환경 만들기
+# conda create -n 가상환경이름
+# conda create -name 가상환경이름
+#  'y' 입력
+
+# 가상환경 삭제
+
+
+# 가상환경
+# - 활성화
+# conda activate 가상환경이름
+# - 비활성화
+# conda deactivate
+
+# 패키지 설치
+# conda install 패키지명
+# ex) conda install matplotlib
